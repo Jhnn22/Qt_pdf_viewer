54028149f56481dc00f80b8e09e3f41ccdd18cfb
Jhnn22 <jhnn22@naver.com>
2024-11-08 00:52:59 +0900

    feat: pdf 불러오기 기능 추가

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -1,14 +1,60 @@
 #include "mainwidget.h"
 #include "./ui_mainwidget.h"
+#include <QPdfView>
+#include <QPdfDocument>
+#include <QPdfPageNavigator>
+#include <QFileDialog>
+#include <QStandardPaths>
 
 MainWidget::MainWidget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::MainWidget)
+    , m_pdfView(new QPdfView(this)), m_pdfDocument(new QPdfDocument(this))
 {
     ui->setupUi(this);
+    ui->verticalLayout->addWidget(m_pdfView);
+
+    m_pdfView->setDocument(m_pdfDocument);
+    m_pdfView->setDocumentMargins(QMargins(0, 0, 0, 0));
+    m_pdfView->setPageMode(QPdfView::PageMode::MultiPage);
+
+    setConnects();
 }
 
 MainWidget::~MainWidget()
 {
     delete ui;
+}
+
+void MainWidget::setConnects(){
+    connect(ui->load_pushButton, &QPushButton::clicked, this, [this](){
+        if(m_fileDialog == nullptr){
+            m_fileDialog = new QFileDialog(this, tr("Pdf 불러오기"), QStandardPaths::writableLocation(QStandardPaths::DownloadLocation));
+            m_fileDialog->setAcceptMode(QFileDialog::AcceptOpen);
+            m_fileDialog->setMimeTypeFilters({"application/pdf"});
+        }
+        if(m_fileDialog->exec() == QDialog::Accepted){
+            const QUrl toOpen = m_fileDialog->selectedUrls().constFirst();
+            if(toOpen.isValid()){
+                open(toOpen);
+                qDebug() << "open";
+            }
+        }
+
+    });
+}
+
+void MainWidget::open(const QUrl &pdfLocation){
+    if(pdfLocation.isLocalFile()){
+        m_pdfDocument->load(pdfLocation.toLocalFile());
+        pageSelected(0);
+    }
+    else{
+        qDebug() << "open error";
+    }
+}
+
+void MainWidget::pageSelected(int page){
+    auto nav = m_pdfView->pageNavigator();
+    nav->jump(page, {}, nav->currentZoom());
 }
--- mainwidget.h
+++ mainwidget.h
@@ -2,6 +2,11 @@
 #define MAINWIDGET_H
 
 #include <QWidget>
+#include <QUrl>
+
+class QPdfView;
+class QPdfDocument;
+class QFileDialog;
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,5 +24,15 @@ public:
 
 private:
     Ui::MainWidget *ui;
+
+    QPdfView *m_pdfView;
+    QPdfDocument *m_pdfDocument;
+    QFileDialog *m_fileDialog = nullptr;
+
+
+private:
+    void setConnects();
+    void open(const QUrl &pdfLocation);
+    void pageSelected(int page);
 };
 #endif // MAINWIDGET_H
